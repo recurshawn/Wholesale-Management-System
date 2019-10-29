e67671940040a18d1f1bfb7d58b29c1b665040e7
shaikharfan7 <shaikharfan7@gmail.com>
2019-10-30 00:50:06 +0530

    commit

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 
 var con = mysql.createConnection({
@@ -22,23 +20,13 @@ con.connect(function(err) {
   });
 });
 
-var 
+
 
 /*GET PRODUCT FORM*/
 router.get('/', function(req, res, next) {
 	res.render('product', {title:'Add Product Details', data: products});
 });
  
-router.post('/thank', urlencodedParser, function (req, res){
-  var reply='';
-  reply += "Product ID    : " + req.body.pid;
-  reply += "Product Name  : " + req.body.pname; 
-  reply += "Buying Price  : " + req.body.bprice;
-  reply += "Selling Price : " + req.body.sprice;
-  reply += "Quantity      : " + req.body.quan;
-  reply += "Category      : " + req.body.cat;
-  res.send(reply);
- });
 
 
 
