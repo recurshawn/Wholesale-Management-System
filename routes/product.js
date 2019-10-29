e1fe08174e3f7a0f45ef3e4a3750bbd27c639278
Shawn Pereira <recurshawn@users.noreply.github.com>
2019-10-29 23:51:47 +0530

    Fixed bug

--- routes/product.js
+++ routes/product.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /*GET PRODUCT FORM*/
-router.get('/',function(req, res, next){
-	res.render('product',{title:'Add Product Details'});
+router.get('/', function(req, res, next) {
+	res.render('product', {title:'Add Product Details'});
 });
 
 
-module.exports=router;
+module.exports  = router;
