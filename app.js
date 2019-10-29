1d3b3a08ba57f0cf005680f5a36d6a79ecc5fdb4
shaikharfan7 <shaikharfan7@gmail.com>
2019-10-29 23:32:00 +0530

    Added product

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productRouter = require('./routes/product')
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
