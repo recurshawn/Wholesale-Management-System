3ce76e199aadee496407f9501968ad096b28fb7d
Shawn Pereira <recurshawn@users.noreply.github.com>
2019-10-30 00:37:01 +0530

    Getting Product Data

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wholesalemgmt"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM product", function (err, result, fields) {
+	if (err) throw err;
+	var products= result;
+    console.log(result);
+  });
+});
+
+var 
 /*GET PRODUCT FORM*/
 router.get('/', function(req, res, next) {
-	res.render('product', {title:'Add Product Details'});
+	res.render('product', {title:'Add Product Details', data: products});
 });
 
 
