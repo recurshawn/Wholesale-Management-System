a6cc402fec3de4c9fba62908e2dcf1767e664531
Shawn Pereira <recurshawn@users.noreply.github.com>
2019-11-03 17:26:40 +0530

    Working on category search for Product

--- routes/product_search.js
+++ routes/product_search.js
@@ -5,33 +5,22 @@ var mysql = require('mysql');
 var conn = require('../data/conn');
 
 var products;
-var quan;
+
 conn.connect(function (err) {
 	if (err) throw err;
 });
 
-router.get('/', function (req, res, next) {
+router.post('/', function (req, res, next) {
 	var qdata = url.parse(req.url, true);
 	var q = qdata.query;
-	//console.log(q);
-	if(q.quantity)
-	{
-		quan = parseInt(req['body'].quantity);
-		conn.query("SELECT * FROM product WHERE quantity >= " + quan, function (err, result, fields) {
-			if (err) throw err;
-			products = result;
-			console.log(result);
-			res.render('product', { title: 'Inventory', data: products, success: {add: q.add, delete: q.delete, update: q.update, prod: q.prod } });
-		});
-	}
-	else {
-	conn.query("SELECT * FROM product", function (err, result, fields) {
+    var quan;
+    if(q.quantity)
+	conn.query("SELECT * FROM product WHERE quantity >= " +quan, function (err, result, fields) {
 		if (err) throw err;
 		products = result;
 		console.log(result);
 		res.render('product', { title: 'Inventory', data: products, success: {add: q.add, delete: q.delete, update: q.update, prod: q.prod } });
 	});
-}
 });
 
 module.exports = router;
