daff6274f8b1c6c72002bd146eaca92628bb7aa6
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2019-11-04 14:35:25 +0530

    Added delete

--- routes/product_delete.js
+++ routes/product_delete.js
@@ -13,38 +13,11 @@ const conn = mysql.createConnection({
 var prods;
 var sql;
 router.post('/', (req, res, next) => {
-
-	var abc2 = "'" + req['body'].pname + "'";
-	var abc3 = parseInt(req['body'].buying_price);
-	var abc4 = parseInt(req['body'].selling_price);
-	var abc5 = parseInt(req['body'].quantity);
-	var abc6 = "'" + req['body'].category + "'";
-
-	prods = [abc2, abc3, abc4, abc5, abc6];
-
-	// console.log(product);
-
-	console.log('Inserting');
-	sql = "INSERT INTO product (pname,buying_price,selling_price,quantity,category) values (" + prods + ");";
-
-
-	conn.connect(function (err) {
+	console.log(`WARNING!!! Deleting product #${req.body.id}`)
+	conn.query("DELETE FROM product WHERE product_id = ?", req.body.id, (err, results) => {
 		if (err) throw err;
-		console.log("connected");
-
-
-		conn.query(sql, function (err, result) {
-			if (err) throw err;
-			console.log(2);
-			console.log("1 record inserted");
-			console.log(result);
-			res.redirect('/product?add=true&prod=' + abc2);
-			//res.render('product', {title: 'Inventory', success: { add: true, prod : abc2}});
-		});
-	});
-
-
-
+		res.redirect('/product')
+	})
 });
 
 
