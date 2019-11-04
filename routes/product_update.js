3233904ddef9104e75ab5d128fc7063b7232a896
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2019-11-04 14:28:37 +0530

    Create update function (not connected to buttons)

--- routes/product_update.js
+++ routes/product_update.js
@@ -12,41 +12,33 @@ const conn = mysql.createConnection({
 
 var prods;
 var sql;
+// DISPLAY EDIT FORM
+router.get('/', (req, res, next) => {
+	console.log(`Updating product ${req.query.id}`)
+	conn.query("SELECT * FROM product WHERE product_id = ?", [req.query.id], (err, result) => {
+		if (err) throw err
+		console.log(result[0])
+		res.render('product_update', {title: `Update Product #${result[0].product_id}`, main: result[0]});
+	})
+});
+
+// PERFORM UPDATE
 router.post('/', (req, res, next) => {
-
-    console.log(req['body'].value);
-	// var abc2 = "'" + req['body'].pname + "'";
-	// var abc3 = parseInt(req['body'].buying_price);
-	// var abc4 = parseInt(req['body'].selling_price);
-	// var abc5 = parseInt(req['body'].quantity);
-	// var abc6 = "'" + req['body'].category + "'";
-
-	// prods = [abc2, abc3, abc4, abc5, abc6];
-
-
-	// console.log(product);
-
-	console.log('Inserting');
-	sql = "INSERT INTO product (pname,buying_price,selling_price,quantity,category) values (" + prods + ");";
-
-
-	conn.connect(function (err) {
-		if (err) throw err;
-		console.log("connected");
-
-
-		conn.query(sql, function (err, result) {
-			if (err) throw err;
-			console.log(2);
-			console.log("1 record inserted");
-			console.log(result);
-			res.redirect('/product?add=true&prod=' + abc2);
-			//res.render('product', {title: 'Inventory', success: { add: true, prod : abc2}});
-		});
-	});
-
-
-
+	console.log(req)
+	console.log(`Recieved fro updating ${req.body.product_id}`)
+	var product = {
+        product_id: req['body'].product_id,
+        pname: req['body'].pname,
+        buying_price: req['body'].buying_price,
+		selling_price: req['body'].selling_price,
+		quantity: req['body'].quantity,
+        category: req['body'].category
+    }
+	conn.query(`UPDATE product SET ? WHERE product_id = ${product.product_id}`, product, (err, results) => {
+		if (err) throw err
+		console.log('Update product', results[0])
+		res.redirect('/product')
+	})
 });
 
 
